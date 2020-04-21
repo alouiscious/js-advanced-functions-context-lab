5e22c50780bfba84a028e081bfb2bfaa94f44834
Alouiscious Media <info@reneesoul.com>
2020-04-21 01:59:18 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,71 @@
-/* Your Code Here */
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function (empRecordArr){
+    return empRecordArr.map(function(row){
+        return createEmployeeRecord(row)
+    }) 
+}
+
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(qryDate){
+    let inTime = this.timeInEvents.find(function(event){
+        return event.date === qryDate
+    })
+
+    let outTime = this.timeOutEvents.find(function(event){
+        return event.date === qryDate
+    })
+
+    return (outTime.hour - inTime.hour) / 100
+}
+
+let wagesEarnedOnDate = function(qryDate){
+    let payWage = hoursWorkedOnDate.call(this, qryDate) * this.payPerHour
+    return payWage
+}
+
+let calculatePayroll = function(empRecordArr){
+    return empRecordArr.reduce(function(memo, payAmount){
+        return memo + allWagesFor.call(payAmount)
+    }, 0)
+}
+
+let findEmployeeByFirstName = function(empRecordArr, firstName){
+    return empRecordArr.find(function(empRecordItem){
+        return empRecordItem.firstName === firstName
+    })
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
